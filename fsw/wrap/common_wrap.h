8d2e0b21858b04eba000f4caafc2831689df397d
Marshall, James C 459937851 <james.marshall-1@nasa.gov>
2022-12-16 09:38:25 -0500

    Update copyright and license

--- fsw/wrap/common_wrap.h
+++ fsw/wrap/common_wrap.h
@@ -1,10 +1,9 @@
 /*
 ** GSC-18364-1, "Proxy Core Flight System Application and Client for External Process"
 **
-** Copyright © 2019 United States Government as represented by
+** Copyright © 2019-2022 United States Government as represented by
 ** the Administrator of the National Aeronautics and Space Administration.
-** No copyright is claimed in the United States under Title 17, U.S. Code.
-** All Other Rights Reserved.
+** All Rights Reserved.
 **
 ** Licensed under the NASA Open Source Agreement version 1.3
 ** See "NOSA GSC-18364-1.pdf"
